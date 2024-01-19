db5ddd5d2c202e806669eb5ef848a2a755be3244
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-01-18 22:25:25 -0300

    Teste

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Ola mundo");
+        for(int i = 0; i<=5;i++){
+        System.out.println("Ola mundo");}
     }
 }
